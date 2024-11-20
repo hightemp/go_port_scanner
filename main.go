a9a059baa14358810aa5c8809c7dc2f1305224df
hightemp <hightemp.unknown@gmail.com>
2024-11-20 13:00:14 +0300

    update

--- main.go
+++ main.go
@@ -9,11 +9,9 @@ import (
 )
 
 var (
-	portsChan     chan int
-	resultTCPChan chan int
-	resultUDPChan chan int
-	hostname      *string
-	wg            sync.WaitGroup
+	portsChan chan int
+	hostname  *string
+	wg        sync.WaitGroup
 )
 
 func worker() {
@@ -21,69 +19,40 @@ func worker() {
 	for port := range portsChan {
 		host := fmt.Sprintf("%s:%d", *hostname, port)
 
-		//wg.Add(2)
-		//go func() {
-		fmt.Printf("checking tcp: %s\n", host)
-		conn, err := net.DialTimeout("tcp", host, time.Second*1)
+		d := net.Dialer{Timeout: time.Second}
+		conn, err := d.Dial("tcp", host)
 		if err == nil {
-			resultTCPChan <- port
+			fmt.Printf("TCP: %d\n", port)
 			conn.Close()
 		}
-		//	wg.Done()
-		//}()
-
-		//go func() {
-		fmt.Printf("checking upd: %s\n", host)
-		conn, err = net.DialTimeout("udp", host, time.Second*1)
-		if err == nil {
-			resultUDPChan <- port
-			conn.Close()
-		}
-		//	wg.Done()
-		//}()
 	}
 }
 
 func main() {
 	portsChan = make(chan int, 100)
-	resultTCPChan = make(chan int, 100)
-	resultUDPChan = make(chan int, 100)
 
 	hostname = flag.String("host", "localhost", "Hostname or IP address")
+	numWorkers := flag.Int("workers", 10000, "Number of concurrent workers")
+	startPort := flag.Int("start", 1, "Start port")
+	endPort := flag.Int("end", 65535, "End port")
 	flag.Parse()
 
-	numWorkers := 10000 // runtime.NumCPU() * 4
-	for i := 0; i < numWorkers; i++ {
+	portCount := *endPort - *startPort + 1
+	if *numWorkers > portCount {
+		*numWorkers = portCount
+	}
+
+	for i := 0; i < *numWorkers; i++ {
 		wg.Add(1)
 		go worker()
 	}
 
 	go func() {
-		for i := 1; i <= 65535; i++ {
+		for i := *startPort; i <= *endPort; i++ {
 			portsChan <- i
 		}
 		close(portsChan)
 	}()
 
-	done := make(chan bool)
-
-	go func() {
-		var openTCPPorts []int
-		for port := range resultTCPChan {
-			openTCPPorts = append(openTCPPorts, port)
-		}
-		fmt.Printf("\nTCP: %v\n", openTCPPorts)
-		var openUDPPorts []int
-		for port := range resultUDPChan {
-			openUDPPorts = append(openUDPPorts, port)
-		}
-		fmt.Printf("\nUDP: %v\n", openUDPPorts)
-		done <- true
-	}()
-
 	wg.Wait()
-
-	close(resultTCPChan)
-	close(resultUDPChan)
-	<-done
 }
